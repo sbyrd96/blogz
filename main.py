762974a94d65166017c8eb5c83351efd13d33cf5
Susan Byrd <byrd96@outlook.com>
2019-05-23 11:27:01 -0500

    finished build a blog project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@l
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+app.secret_key = "secretkey"
 
 class Entry(db.Model):
 
@@ -35,11 +36,16 @@ def index():
 #create a main page that also lists all entries
 
 @app.route('/blog', methods=['POST', 'GET'])
-    def all_entries():
-        entry_id = request.args.get('id')
-        if (entry_id):
-            Entry.query.get(entry_id)
-            return render_template('blog.html', title="Blog Post", entry=entry)
+def all_entries():
+
+    entry_id = request.args.get('id')
+    if (entry_id):
+        entry = Entry.query.get(entry_id)
+        return render_template('completed_entry.html', title="My Blog Post", entry=entry)
+    else: 
+     entries = Entry.query.all()       
+
+    return render_template('blog.html', title="Build a Blog", entries=entries)
 
 
 # page to enter text to create a new blogpost
@@ -49,32 +55,19 @@ def new_entry():
     if request.method == 'POST':
         new_entry_title = request.form['title']
         new_entry_body = request.form['body']
-
         new_entry = Entry(new_entry_title, new_entry_body)
 
         if new_entry.completed_entry():
             db.session.add(new_entry)
             db.session.commit()
+            return redirect("/blog?id=" + str(new_entry.id))
 
-            url = "/blog?id=" + str(new_entry.id)
-            return redirect(url)
         else:
             flash("Both a title and a blog entry are required. Please fill both in.")
-            return render_template('/newentry.html',title="Create New Blog Entry", new_entry=new_entry, new_entry_title=new_entry_title, new_entry_body=new_entryk_body)
+            return render_template('/newpost.html',title="Create New Blog Entry", new_entry=new_entry, new_entry_title=new_entry_title, new_entry_body=new_entry_body)
 
-    return render_template('/completed_entry.html',title="Blog Entry", new_entry_title=new_entry_body, new_entry_title=new_entry_title)     
-
-
-# create new page that lists the selected (or newest) entry
-
-@app.route("/completed_entry, methods=["GET"])
-def completed_entry():
-    completed_entry_id = request.form['completed_entry']
-
-    completed_entry = Entry.query.get(completed_entry_id)
-    return render_template('completed_entry') 
-
-
+    else:
+        return render_template('/newpost.html',title="Blog Entry")     
 
 
 if __name__ == '__main__':
